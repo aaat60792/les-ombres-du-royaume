9f7bcf1e1a4ee05402622171c248f825de0e09b4
aaat60792 <aaat60792@gmail.com>
2026-07-03 18:15:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ p.classList.remove('active');
 document.getElementById(id).classList.add('active');
 }
 
-/* search */
+/* SEARCH */
 document.getElementById("search").addEventListener("input",(e)=>{
 let v=e.target.value.toLowerCase();
 
@@ -15,3 +15,36 @@ c.style.display = c.innerText.toLowerCase().includes(v)
 : "none";
 });
 });
+
+/* RPG DATA */
+const roles = {
+roi: `
+<h2>👑 Roi des Ombres</h2>
+<p><b>Camp :</b> Ombres</p>
+<p>Élimine un joueur chaque nuit.</p>
+<p>Victoire : survivre jusqu’à la fin.</p>
+`,
+
+garde: `
+<h2>🛡 Garde</h2>
+<p><b>Camp :</b> Royaume</p>
+<p>Protège un joueur chaque nuit.</p>
+<p>Ne peut pas se protéger lui-même.</p>
+`,
+
+voyant: `
+<h2>🔮 Voyant</h2>
+<p>Détecte si une Ombre est parmi 2 joueurs.</p>
+`
+};
+
+/* OPEN POPUP */
+function openRole(id){
+document.getElementById("popup-body").innerHTML = roles[id];
+document.getElementById("popup").classList.remove("hidden");
+}
+
+/* CLOSE POPUP */
+function closePopup(){
+document.getElementById("popup").classList.add("hidden");
+}
